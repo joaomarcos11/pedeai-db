43441d4fa02ea40c92724257e603ee4f281b8a80
Murilo Martins <murilomartins@outlook.com>
2024-12-02 17:30:51 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "mongodb_atlas_public_key" {
+  description = "MongoDB Atlas Public Key"
+  type        = string
+}
+
+variable "mongodb_atlas_private_key" {
+  description = "MongoDB Atlas Private Key"
+  type        = string
+}
+
+variable "mongodb_atlas_org_id" {
+  description = "MongoDB Atlas Organization ID"
+  type        = string
+}
